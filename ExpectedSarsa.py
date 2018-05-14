3e08e7bfd56b19f900e86685541256ea9092e2fd
chasemc67 <chasemc67@gmail.com>
2018-05-13 19:36:47 -0700

    Putting in new repo

---
+++ ExpectedSarsa.py
@@ -0,0 +1,112 @@
+from __future__ import division
+import blackjack
+import random
+from random import randint
+
+
+#== Globals
+Q = [[0 for x in range(2)]for x in range(181)]
+
+numEpisodes = 110000000
+
+dropEpsilonEpisode = 1000000
+dropAlpha = 2500000
+epsilon = 0.01
+alpha = 0.05
+
+gamma = 1
+returnSum = 0
+
+originalEpsilon = epsilon
+originalAlpha = alpha
+
+#initialize Q to small random number
+for i in range(len(Q)):
+	for j in range(len(Q[i])):
+		Q[i][j] = random.random() * 0.001
+
+
+# Prints out the action values for every state-action pair
+def printActionValues(Q):
+	print("Action Values:")
+	for S in range(181):
+		print str(blackjack.visualDecode(S)) + " Stay: " + str(Q[S][0])
+		print str(blackjack.visualDecode(S)) + " Hit: " + str(Q[S][1])
+
+# Prints out the learned policy
+def printPolicy(Q):
+	print "policy: "
+	for S in range(181):
+		if Q[S][0] >= Q[S][1]:
+			print str(blackjack.visualDecode(S)) + " Stay"
+		else:
+			print str(blackjack.visualDecode(S)) + " Hit"
+
+#Used as an argument to blackjack.printpolicy()
+def showPolicy(S):
+	if Q[S][0] >= Q[S][1]:
+		return 0
+	else:
+		return 1
+
+def printSettings():
+	print "Settings:"
+	print "Episodes: " + str(numEpisodes)
+	print "Drop Epsilon: " + str(dropEpsilonEpisode)
+	print "Epsilon: " + str(originalEpsilon)
+	print "Alpha: " + str(originalAlpha)
+
+
+#== Main
+for episodeNum in range(numEpisodes):
+	S = blackjack.init()
+	G = 0
+
+	#while S is not in terminal state
+	while S != -1:
+
+		#Choose action here based on epsilon
+		decider = random.random()
+		if decider <= epsilon:
+			A = randint(0,1)
+		else:
+			# A = the best action to take
+			if Q[S][0] >= Q[S][1]:
+				A = 0
+			else:
+				A = 1
+
+		R,Sprime = blackjack.sample(S,A)
+		G = G + R
+
+		if episodeNum > dropAlpha:
+			print R
+
+
+		if Sprime == -1:
+			Q[S][A] = Q[S][A] + alpha*(R - Q[S][A])	
+		else:
+			Q[S][A] = Q[S][A] + alpha*(R + gamma*(max(Q[Sprime][0],Q[Sprime][1])) - Q[S][A])
+		
+		S = Sprime
+
+	if episodeNum == dropEpsilonEpisode:
+		#print "=============================END EXPLORING PHASE============================="
+		epsilon = 0
+
+	if episodeNum == dropAlpha:
+		#print "=============================END LEARNING PHASE============================="
+		alpha = 0
+
+	#if episodeNum % 10000 == 0:
+		#print "Current Avg: " + str(returnSum / (episodeNum+1)) + " Ep: " + str(episodeNum)
+
+	returnSum = returnSum + G
+
+#blackjack.printPolicy(showPolicy)
+
+#print ""
+#print "Avg Return:" + str(returnSum / numEpisodes)	
+	
+#printSettings()
+
